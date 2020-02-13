5e3b780b8a65be284e57dc3e522e3eb1dbd5166c
mmiah00 <mmiah00@stuy.edu>
2020-02-13 16:43:28 +0000

    wrote identity need to write multiplication

--- matrix.py
+++ matrix.py
@@ -12,28 +12,31 @@ import math
 #print the matrix such that it looks like
 #the template in the top comment
 def print_matrix( matrix ):
-    row1, row2, row3,row4 = "","", ""
+    row1, row2, row3,row4 = "","", "", ""
     for i in range (len (matrix)):
-        row1 += matrix[i][0] + " "
-        row2 += matrix[i][1] + " "
-        row3 += matrix[i][2] + " "
-        row4 += matrix[i][3] + " "
-    row1 += "\n"
-    row2 += "\n"
-    row3 += "\n"
-    row4 += "\n"
-
-    print (row1 + row2 + row3 + row4)
+        row1 += str (matrix[i][0]) + " "
+        row2 += str (matrix[i][1]) + " "
+        row3 += str (matrix[i][2]) + " "
+        row4 += str (matrix[i][3]) + " "
+    print (row1)
+    print (row2)
+    print (row3)
+    print (row4)
 
 #turn the paramter matrix into an identity matrix
 #you may assume matrix is square
 def ident( matrix ):
-    pass
+    for i in range (len(matrix)):
+        for j in range (len (matrix[i])):
+            if i == j:
+                matrix[i][j] = 1
+            else:
+                matrix[i][j] = 0
 
 #multiply m1 by m2, modifying m2 to be the product
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
+
 
 
 
@@ -45,3 +48,18 @@ def new_matrix(rows = 4, cols = 4):
         for r in range( rows ):
             m[c].append( 0 )
     return m
+
+a = new_matrix ()
+a[0] = [21,31,41,51]
+a[1] = [2,4,7,1]
+a[2] = [20, 10, 0, 2]
+a[3] = [1,1,1,1]
+
+print ("Matrix RN:\n")
+print_matrix (a)
+print ("\n")
+
+print ("Now It's an Identity Matrix:\n")
+ident (a)
+print_matrix (a)
+print ("\n")
